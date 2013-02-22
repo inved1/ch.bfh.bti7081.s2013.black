dec064055bfb6df105d74770973517bad687e0ee
villj2 <villj2@bfh.ch>
2013-02-22 22:38:47 +0100

    test netbeans

--- ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java
+++ ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java
@@ -1,11 +1,14 @@
 package test;
 
 public class HelloWorld {
-	
-	
-	
-	
-	
-	
-	
+
+    public HelloWorld() {
+        
+        
+    }
+    
+    public static void main(String[] args){
+        
+        System.out.println("Inversini sux dix");
+    }
 }
