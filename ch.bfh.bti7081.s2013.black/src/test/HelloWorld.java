9ce29293a21ee43d29d4da1e4581fbc44efd4c6b
villj2 <villj2@bfh.ch>
2013-02-22 23:18:21 +0100

    Update ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java

--- ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java
+++ ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
     
     public static void main(String[] args){
         
-        // edited online, yeah yeah yeah
+        // asdjfla djfladj flakjdf lasjdf la
         
         System.out.println("Inversini sux dix");
     }
