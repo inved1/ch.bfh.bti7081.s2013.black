b8562c64f4a61cdeed13d72f16f299705f2e5381
inved1 <inved1@bfh.ch>
2013-02-25 19:27:46 +0100

    huhu

--- ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java
+++ ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java
@@ -8,6 +8,6 @@ public class HelloWorld {
     
     public static void main(String[] args){
         
-        System.out.println("Inversini sux dix");
+        System.out.println("Inversini rockt");
     }
 }
