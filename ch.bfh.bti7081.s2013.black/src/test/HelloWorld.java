254fd44aeacfb46c6690a1494c429d88a8ef18f3
Marco Füllemann <mfuellemann@gmail.com>
2013-02-25 19:36:32 +0100

    correction

--- ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java
+++ ch.bfh.bti7081.s2013.black/src/test/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
     
     public static void main(String[] args){
         
-        System.out.println("Inversini rockt");
+        System.out.println("Inversini rockt oder so...");
         
         
     }
