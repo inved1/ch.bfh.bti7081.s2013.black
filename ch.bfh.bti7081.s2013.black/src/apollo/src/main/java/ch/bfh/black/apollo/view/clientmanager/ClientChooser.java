a62f3d15a88b87751d5f05547703b11fd78efbb6
inved1 <inved1@bfh.ch>
2013-05-15 18:45:32 +0200

    added select to "clientChooser"

--- ch.bfh.bti7081.s2013.black/src/apollo/src/main/java/ch/bfh/black/apollo/view/clientmanager/ClientChooser.java
+++ ch.bfh.bti7081.s2013.black/src/apollo/src/main/java/ch/bfh/black/apollo/view/clientmanager/ClientChooser.java
@@ -7,6 +7,7 @@ package ch.bfh.black.apollo.view.clientmanager;
 import ch.bfh.black.apollo.controller.clientmanager.ClientManagerController;
 import ch.bfh.black.apollo.model.data.Client;
 import ch.bfh.black.apollo.view.ContentHelper;
+import com.vaadin.data.Container;
 import com.vaadin.data.Property;
 import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
@@ -47,60 +48,71 @@ public class ClientChooser extends AbsoluteLayout implements View {
             ArrayList<Client> lst = Client.listAll();
             System.out.println(lst.toString());
             
+       
+            
+       
+            
+        
+        
+
+            // TABLE
+            Table table = new Table("Clients");
+           
+
+            // Define two columns for the built-in container
+            table.addContainerProperty("Name", String.class, null);
+            table.addContainerProperty("Nr",  Integer.class, null);
+            
+            for(Client cl: lst){
+                table.addItem(new Object[]{cl.getName1(),cl.getClientID()},lst.indexOf(cl)+1);
+            }
+            /*
+            table.addItem(new Object[]{"Julien Villiger", 3333}, 1);
+            table.addItem(new Object[]{"Daniel Inversini", 3184}, 2);
+            table.addItem(new Object[]{"Fanky", 3648}, 3);
+            table.addItem(new Object[]{"Fülli", 3009}, 4);
+            table.addItem(new Object[]{"Mao Tse-Tung", 5938}, 5);
+*/
+            // Show 5 rows
+            table.setPageLength(5);
+            table.setSelectable(true);
+            table.setImmediate(true);
+            table.setSizeFull();
+            table.setStyleName("client-chooser-table");
+
+            // Handle selection changes
+
+            table.addValueChangeListener(new Property.ValueChangeListener() {
+
+                @Override
+                public void valueChange(Property.ValueChangeEvent event) {
+
+                    Notification.show(event.getProperty().getValue().toString());
+                }
+            });
+            addComponent(table);
+
+
+            Button button = new Button("back");
+            button.addClickListener(new Button.ClickListener() {
+
+                @Override
+                public void buttonClick(Button.ClickEvent event) {
+
+                    _cmc.chooseClient();
+                }
+            });
+            addComponent(button);
+            button.setStyleName("client-chooser-bt-back");
+
+
+            _ch.drawFooter();
+        
+             
+            
         } catch (SQLException ex) {
             Logger.getLogger(ClientChooser.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
-        
-        
-        
-        // TABLE
-        Table table = new Table("Clients");
-        
-        // Define two columns for the built-in container
-        table.addContainerProperty("Name", String.class, null);
-        table.addContainerProperty("Nr",  Integer.class, null);
-
-        table.addItem(new Object[]{"Julien Villiger", 3333}, 1);
-        table.addItem(new Object[]{"Daniel Inversini", 3184}, 2);
-        table.addItem(new Object[]{"Fanky", 3648}, 3);
-        table.addItem(new Object[]{"Fülli", 3009}, 4);
-        table.addItem(new Object[]{"Mao Tse-Tung", 5938}, 5);
-
-        // Show 5 rows
-        table.setPageLength(5);
-        table.setSelectable(true);
-        table.setImmediate(true);
-        table.setSizeFull();
-        table.setStyleName("client-chooser-table");
-        
-        // Handle selection changes
-       
-        table.addValueChangeListener(new Property.ValueChangeListener() {
-        
-            @Override
-            public void valueChange(Property.ValueChangeEvent event) {
-                
-                Notification.show(event.getProperty().getValue().toString());
-            }
-        });
-        addComponent(table);
-        
-        
-        Button button = new Button("back");
-        button.addClickListener(new Button.ClickListener() {
-            
-            @Override
-            public void buttonClick(Button.ClickEvent event) {
-                
-                _cmc.chooseClient();
-            }
-        });
-        addComponent(button);
-        button.setStyleName("client-chooser-bt-back");
-        
-        
-        _ch.drawFooter();
         
     }
     
