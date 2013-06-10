42fc68c9f20daf51a4109b3a0a8a1d91624f5d09
Marco Füllemann <mfuellemann@gmail.com>
2013-06-10 19:03:13 +0200

    applied suggestion from find bugs

--- ch.bfh.bti7081.s2013.black/src/apollo/src/main/java/ch/bfh/black/apollo/model/Dict.java
+++ ch.bfh.bti7081.s2013.black/src/apollo/src/main/java/ch/bfh/black/apollo/model/Dict.java
@@ -7,25 +7,25 @@ package ch.bfh.black.apollo.model;
  */
 public class Dict {
     
-    public static String DATE = "29.05.2013";
-    public static String GROUP_NAME = "black";
+    public static final String DATE = "29.05.2013";
+    public static final String GROUP_NAME = "black";
     
-    public static String HEADER_MAIN_TITLE = "Apollo";
+    public static final String HEADER_MAIN_TITLE = "Apollo";
     
-    public static String MAIN_MENU_TITLE = "Choose module.";
-    public static String MAIN_MENU_CM = "Client Manager";
-    public static String MAIN_MENU_PT = "Feature 2";
-    public static String MAIN_MENU_XX = "Feature 3";
-    public static String MAIN_MENU_YY = "Feature 4";
+    public static final String MAIN_MENU_TITLE = "Choose module.";
+    public static final String MAIN_MENU_CM = "Client Manager";
+    public static final String MAIN_MENU_PT = "Feature 2";
+    public static final String MAIN_MENU_XX = "Feature 3";
+    public static final String MAIN_MENU_YY = "Feature 4";
     
-    public static String CLIENT_CHOOSER_TITLE = "Clients.";
+    public static final String CLIENT_CHOOSER_TITLE = "Clients.";
     
-    public static String CLIENT_DETAIL_TITLE = "Client-Details.";
-    public static String CLIENT_DETAIL_HISTORY_TITLE = "Client-History.";
+    public static final String CLIENT_DETAIL_TITLE = "Client-Details.";
+    public static final String CLIENT_DETAIL_HISTORY_TITLE = "Client-History.";
     
-    public static String ADD_CLIENT_HISTORY_TITLE = "Add Client History Entry.";
-    public static String ADD_CLIENT_HISTORY_DATE_TITLE = "Enter Date.";
-    public static String ADD_CLIENT_HISTORY_TEXT_TITLE = "Enter Text.";
+    public static final String ADD_CLIENT_HISTORY_TITLE = "Add Client History Entry.";
+    public static final String ADD_CLIENT_HISTORY_DATE_TITLE = "Enter Date.";
+    public static final String ADD_CLIENT_HISTORY_TEXT_TITLE = "Enter Text.";
     
-    public static String DATE_NOT_CORRECT = "Date is not correct.";
+    public static final String DATE_NOT_CORRECT = "Date is not correct.";
 }
